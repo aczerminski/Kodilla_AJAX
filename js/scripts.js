31692a9cb8f77211ce8683cb1187234e19550a4e
artur czerminski <czermi@gmail.com>
2017-08-01 08:15:05 +0200

    modified[p

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 var url = 'https://restcountries.eu/rest/v1/name/';
+var countriesDetails = 'https://restcountries.eu/rest/v2/all';
 var countriesList = $('#countries');
 
 $('#search').click(searchCountries);
+$('#details').click(showCountriesDetails);
 
 function searchCountries() {
  	var countryName = $('#country-name').val();
@@ -17,5 +19,13 @@ function showCountriesList(resp) {
   countriesList.empty();
   resp.forEach(function(item) {
     $('<li>').text(item.name).appendTo(countriesList);
+  });
+}
+
+function showCountriesDetails (){
+  $.ajax({
+    url: countriesDetails,
+    method: 'GET',
+    success: showCountriesDetails
   });
 }
